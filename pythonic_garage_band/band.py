245693c36fcb347e445235cef428b5d87beb2b3a
Dominic Giacona <dominic@exobyte.io>
2022-07-22 02:48:39 -0400

    more work added

--- pythonic_garage_band/band.py
+++ pythonic_garage_band/band.py
@@ -1,3 +1,18 @@
+class Band():
+    instances = []
+
+    def __init__(self, name, arr):
+        self.name = name
+        self.members = arr
+        Band.instances.append(self)
+
+def play_solos(self):
+    return ["face melting guitar solo", "bom bom buh bom", "rattle boom crash"]
+
+    @classmethod
+    def to_(cls):
+        return cls.instances
+
 
 
 class Musician:
